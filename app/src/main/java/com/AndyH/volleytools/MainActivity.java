a2b31b6b285958dce16dc00911f05261273fe278
Anderson Hsieh <andy13579246810@gmail.com>
2021-07-01 10:21:02 -0400

    2021/7/1 UI design for scorer activity, firebase setup

--- app/src/main/java/com/AndyH/volleytools/MainActivity.java
+++ app/src/main/java/com/AndyH/volleytools/MainActivity.java
@@ -1,10 +1,14 @@
 package com.AndyH.volleytools;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,17 +18,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-
-        View view = findViewById(R.id.button_launch_scorer);
-        view.setOnClickListener(new View.OnClickListener() {
+        Button welcomepage_button_scorer = (Button)findViewById(R.id.button_launch_scorer);
+        welcomepage_button_scorer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent scoring_intent = new Intent(v.getContext(), scorer.class);
                 v.getContext().startActivity(scoring_intent);
 
+
+
             }
         });
 
+
+        Button welcomepage_button_history = (Button)findViewById(R.id.button_launch_history);
+        welcomepage_button_history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/AndyH/volleytools/scorer.java
+++ app/src/main/java/com/AndyH/volleytools/scorer.java
@@ -2,13 +2,57 @@ package com.AndyH.volleytools;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.OrientationEventListener;
 
 public class scorer extends AppCompatActivity {
+    CountDownTimer orientation_delay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scorer);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        OrientationEventListener orientationEventListener = new OrientationEventListener(this, SensorManager.SENSOR_DELAY_NORMAL) {
+            @Override
+            public void onOrientationChanged(int orientation) {
+                Log.v("orientation", "Orientation changed to " + orientation);
+                if (orientation == OrientationEventListener.ORIENTATION_UNKNOWN) {
+                    return;
+                }
+
+                int degrees = -1;
+                if (orientation < 45 || orientation > 315) {
+                    Log.i("orientation", "Portrait "+degrees);
+                } else if (orientation < 135) {
+                    degrees = 90;
+                    scorer.super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                    Log.i("orientation", "Landscape "+degrees);    // This can be reverse landscape
+                } else if (orientation < 225) {
+                    degrees = 180;
+                    Log.i("orientation", "Reverse Portrait "+degrees);
+                } else {
+                    degrees = 270;
+                    Log.i("orientation", "Reverse Landscape "+degrees); // This can be landscape
+                    scorer.super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                }
+            }
+        };
+
+        if (orientationEventListener.canDetectOrientation() == true) {
+            Log.v("orientation", "Can detect orientation");
+            orientationEventListener.enable();
+        } else {
+            Log.v("orientation", "Cannot detect orientation");
+            orientationEventListener.disable();
+        }
+
     }
+
 }
