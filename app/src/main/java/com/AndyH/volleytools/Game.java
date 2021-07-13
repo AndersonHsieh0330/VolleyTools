a61198543049b807057b9e488dbf7f751eff59d7
Anderson Hsieh <andy13579246810@gmail.com>
2021-07-13 01:28:44 -0400

    2021/7/13 start working on saveInstancedstate.putParceble, add button onclicklisteners

--- app/src/main/java/com/AndyH/volleytools/Game.java
+++ app/src/main/java/com/AndyH/volleytools/Game.java
@@ -1,8 +1,11 @@
 package com.AndyH.volleytools;
 
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
 
-public class Game {
+public class Game implements Parcelable {
     int points_to_win_set;
     int sets_to_win_game;
     int goodpeople_points;
@@ -30,6 +33,110 @@ public class Game {
         this.badpeople_customize_name = badpeople_customize_name;
     }
 
+    protected Game(Parcel in) {
+        points_to_win_set = in.readInt();
+        sets_to_win_game = in.readInt();
+        goodpeople_points = in.readInt();
+        badpeople_points = in.readInt();
+        goodpeople_sets = in.readInt();
+        badpeople_sets = in.readInt();
+        goodpeople_customize_name = in.readString();
+        badpeople_customize_name = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(points_to_win_set);
+        dest.writeInt(sets_to_win_game);
+        dest.writeInt(goodpeople_points);
+        dest.writeInt(badpeople_points);
+        dest.writeInt(goodpeople_sets);
+        dest.writeInt(badpeople_sets);
+        dest.writeString(goodpeople_customize_name);
+        dest.writeString(badpeople_customize_name);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Game> CREATOR = new Creator<Game>() {
+        @Override
+        public Game createFromParcel(Parcel in) {
+            return new Game(in);
+        }
+
+        @Override
+        public Game[] newArray(int size) {
+            return new Game[size];
+        }
+    };
+
+    public int getPoints_to_win_set() {
+        return points_to_win_set;
+    }
+
+    public void setPoints_to_win_set(int points_to_win_set) {
+        this.points_to_win_set = points_to_win_set;
+    }
+
+    public int getSets_to_win_game() {
+        return sets_to_win_game;
+    }
+
+    public void setSets_to_win_game(int sets_to_win_game) {
+        this.sets_to_win_game = sets_to_win_game;
+    }
+
+    public int getGoodpeople_points() {
+        return goodpeople_points;
+    }
+
+    public void setGoodpeople_points(int goodpeople_points) {
+        this.goodpeople_points = goodpeople_points;
+    }
+
+    public int getBadpeople_points() {
+        return badpeople_points;
+    }
+
+    public void setBadpeople_points(int badpeople_points) {
+        this.badpeople_points = badpeople_points;
+    }
+
+    public int getGoodpeople_sets() {
+        return goodpeople_sets;
+    }
+
+    public void setGoodpeople_sets(int goodpeople_sets) {
+        this.goodpeople_sets = goodpeople_sets;
+    }
+
+    public int getBadpeople_sets() {
+        return badpeople_sets;
+    }
+
+    public void setBadpeople_sets(int badpeople_sets) {
+        this.badpeople_sets = badpeople_sets;
+    }
+
+    public String getGoodpeople_customize_name() {
+        return goodpeople_customize_name;
+    }
+
+    public void setGoodpeople_customize_name(String goodpeople_customize_name) {
+        this.goodpeople_customize_name = goodpeople_customize_name;
+    }
+
+    public String getBadpeople_customize_name() {
+        return badpeople_customize_name;
+    }
+
+    public void setBadpeople_customize_name(String badpeople_customize_name) {
+        this.badpeople_customize_name = badpeople_customize_name;
+    }
+
     public void goodpeople_gain_point(){
         goodpeople_points += 1;
     }
@@ -45,18 +152,29 @@ public class Game {
     public void badpeople_lose_point(){
         badpeople_points -= 1;
     }
-    public void goodpeopl_gain_set(){
+    public void goodpeople_gain_set(){
         goodpeople_sets+=1;
     }
-    public void badpeopl_gain_set(){
+    public void badpeople_gain_set(){
         badpeople_sets+=1;
     }
 
 
-    public void load_bundle_data_in(Bundle bundle){
-        //bundle should either have all the keys, or non of the keys
-
-    }
+//    public void load_bundle_data_in(Bundle bundle){
+//        //bundle should either have all the keys, or non of the keys
+//        if(bundle.containsKey("is_game_on")){
+//            try {
+//                goodpeople_points = bundle.getInt("goodpeople_points");
+//                goodpeople_sets = bundle.getInt("goodpeople_sets");
+//                badpeople_sets = bundle.get("badpeople_sets");
+//            }catch (Exception e){
+//                Log.d("Missing_key", e.getMessage());
+//            }
+//        }else{
+//            throw new RuntimeException("string key not found while loading data");
+//        }
+//
+//    }
 
     public boolean set_end_check(){
         if ((goodpeople_points == points_to_win_set)||(badpeople_points==points_to_win_set)){
--- app/src/main/java/com/AndyH/volleytools/scorer.java
+++ app/src/main/java/com/AndyH/volleytools/scorer.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.PersistableBundle;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.OrientationEventListener;
@@ -36,19 +37,21 @@ public class scorer extends AppCompatActivity {
         // if saveInstanceState has the key "is_game_on", then it is always true
         if(savedInstanceState.containsKey("is_game_on")){
             if(savedInstanceState.getBoolean("is_game_on")==false){
+                //should never end up here
                 throw new RuntimeException("key false state: is_game_on");
             }else{
                 //load unfinished game
                 is_game_on = savedInstanceState.getBoolean("is_game_on");
-                current_game = resort_ongoing_game(savedInstanceState);
+                current_game= savedInstanceState.getParcelable("ongoing_game");
+
             }
         }else{
 
             //create new game with default settings
             savedInstanceState.putBoolean("is_game_on",true);
             this.is_game_on = true;
-            current_game = new Game(25,2,0,
-                    0,0,0,null, null);
+            current_game = new Game(25,2,14,
+                    13,0,0,null, null);
         }
 
 
@@ -91,16 +94,53 @@ public class scorer extends AppCompatActivity {
 
 
         goodpeople_score_button = (Button) this.findViewById(R.id.left_button_score);
+        goodpeople_score_button.setText(current_game.goodpeople_points);
+        goodpeople_score_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current_game.goodpeople_gain_point();
+                goodpeople_score_button.setText(current_game.getGoodpeople_points());
+            }
+        });
+
+
         badpeople_score_button = (Button) this.findViewById(R.id.right_button_score);
+        badpeople_score_button.setText(current_game.badpeople_points);
+        badpeople_score_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current_game.badpeople_gain_point();
+                badpeople_score_button.setText(current_game.getBadpeople_points());
+            }
+        });
+
         goodpeople_set_button = (Button) this.findViewById(R.id.right_button_set);
-        badpeople_score_button = (Button) this.findViewById(R.id.left_button_set);
+        goodpeople_set_button.setText(current_game.goodpeople_sets);
+        goodpeople_set_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current_game.goodpeople_gain_set();
+                goodpeople_set_button.setText(current_game.getGoodpeople_sets());
+            }
+        });
+
+        badpeople_set_button = (Button) this.findViewById(R.id.left_button_set);
+        badpeople_set_button.setText(current_game.badpeople_sets);
+        badpeople_set_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current_game.badpeople_gain_set();
+                badpeople_set_button.setText(current_game.getBadpeople_sets());
+            }
+        });
+
         button_leave = (Button)this.findViewById(R.id.left_button_leave);
         button_leave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), MainActivity.class);
                 startActivity(intent);
-                savedInstanceState.putBoolean("is_game_on", is_game_on);
+                savedInstanceState.putParcelable("ongoing_game",current_game);
 
             }
         });
@@ -111,37 +151,24 @@ public class scorer extends AppCompatActivity {
 
     }
 
-    public Game resort_ongoing_game(Bundle bundle){
-        Game game = new Game();
-        if(bundle.containsKey("is_game_on")){
 
-        }else{
-            //should never get here
-            throw new RuntimeException("bundle key not found");
-        }
 
-        return game;
-    }
+    //    public Game resort_ongoing_game(Bundle bundle){
+//        Game game = new Game();
+//        if(bundle.containsKey("is_game_on")){
+//
+//        }else{
+//            //should never get here
+//            throw new RuntimeException("bundle key not found");
+//        }
+//
+//        return game;
+//    }
 
-    public void onclick_goodpeople_score_add(){
-        current_game.goodpeople_gain_point();
-    }
-    public void onclick_badpeoples_core_add(){
-        current_game.badpeople_gain_point();
-    }
-    public void onclick_goodpeople_set_add(){
-        current_game.goodpeopl_gain_set();
-    }
-    public void onclick_badpeople_set_add(){
-        current_game.badpeopl_gain_set();
-    }
-    public void onclick_leave(){
-
-    }
 
     @Override
-    protected void onDestroy() {
-
-        super.onDestroy();
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        outState.putParcelable("ongoing_game",current_game);
+        super.onSaveInstanceState(outState, outPersistentState);
     }
 }
